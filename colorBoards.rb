c47b844fd7a070c54ed6264bdd5c12a898f3f3a1
anthony_elliott <ellioaac@uni.edu>
2014-03-04 20:20:18 -0600

    Pick highest intersection

--- colorBoards.rb
+++ colorBoards.rb
@@ -9,9 +9,11 @@
 ####################
 
 def driver
-    rows = [1, 0, 0]
-    cols = [1, 0, 0]
-    greedy(rows, cols)
+    rows = [2, 1, 1]
+    cols = [1, 2, 1]
+    #greedy(rows, cols)
+    #puts ""
+    greedy2(rows, cols)
 end
 
 def greedy(rows, cols)
@@ -27,16 +29,43 @@ def greedy(rows, cols)
         end
     end
 
-    puts "Failed" if not board.isValid?
     puts board.to_s
 end
 
+def greedy2(rows, cols)
+    board = Board.new(rows, cols)
+
+    # make deep copies to avoid modifiying the original references
+    newRows = rows.clone
+    newCols = cols.clone
+    placeAtHighestIntersection(board, newRows, newCols)
+end
+
+def placeAtHighestIntersection(board, rows, cols)
+    if rows.max <= 0 or cols.max <= 0
+        puts board.to_s
+        return
+    end
+
+    # pick highest row then highest col and place x there
+    maxRowIndex = rows.index(rows.max)
+    maxColIndex = cols.index(cols.max)
+
+    #puts "Row: " + maxRowIndex.to_s + "\tCol: " + maxColIndex.to_s 
+
+    rows[maxRowIndex] -= 1
+    cols[maxColIndex] -= 1
+
+    board.add(maxRowIndex, maxColIndex)
+
+    return placeAtHighestIntersection(board, rows, cols)
+end
+   
 class Board
     def initialize(rows, cols)
         @rows = rows
         @cols = cols
         @length = rows.length
-        @numTokens = 0
 
         @board = Hash.new(@length)
 
@@ -51,10 +80,12 @@ class Board
     end
     def add(row, col)
         @board[[row,col]] = true
-        @numTokens += 1
     end
-    def numTokens
-        return @numTokens
+    def rows
+        return @rows
+    end
+    def cols
+        return @cols
     end
     def isValid?
         (0..@length - 1).each do |row|
@@ -65,7 +96,10 @@ class Board
                 sum += 1 if @board[[row,col]]
             end
 
-            return false if sum != numTokensExpected
+            if sum != numTokensExpected
+                puts "Failed on row " + row.to_s + ": expected " + numTokensExpected.to_s + " tokens but got " + sum.to_s
+                return false 
+            end
         end
 
         (0..@length - 1).each do |col|
@@ -76,13 +110,18 @@ class Board
                 sum += 1 if @board[[row,col]]
             end
 
-            return false if sum != numTokensExpected
+            if sum != numTokensExpected
+                puts "Failed on col " + col.to_s + ": expected " + numTokensExpected.to_s + " tokens but got " + sum.to_s
+                return false 
+            end
         end
 
         return true
     end
     def to_s
         str = ""
+
+        str += "Failed\n" if not isValid?
 
         (0..@length - 1).each do |row|
             (0..@length - 1).each do |col|
