3e85b9e3d0913be3ff20ef1eb2f2b1851a74dd8f
anthony_elliott <ellioaac@uni.edu>
2014-03-04 18:53:09 -0600

    Framework for board class

--- colorBoards.rb
+++ colorBoards.rb
@@ -17,6 +17,42 @@ end
 def greedy(rows, cols)
     puts "Rows: " + rows.to_s
     puts "Columns: " + cols.to_s
+
+    board = Board.new(rows, cols)
+
+    rows.each_with_index do |row|
+        cols.each_with_index do |col|
+            while row > 0 && col > 0
+                board.add(row, col)
+                row -= 1
+                col -= 1
+            end 
+        end
+    end
+end
+
+class Board
+    def initialize(rows, cols)
+        @rows = rows
+        @cols = cols
+        @length = rows.length
+
+        @board = Hash.new(@length * @length)
+
+        (0..@length).each do |row|
+            (0..@length).each do |col|
+                @board[[row,col]] = false
+            end
+        end
+    end
+    def length
+        return @length
+    end
+    def add(row, col)
+        @board[[row,col]] = true
+    end
+    def to_s
+    end
 end
 
 driver
